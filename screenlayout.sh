9a71e00c529f58aec9db0b4a372c616fe28829de
clyde_nakamoto <109290211+plimdim@users.noreply.github.com>
2022-11-28 02:11:28 +0000

    Update screenlayout.sh

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,1 +1,1 @@
-/plimdim/bash-scripts/change_virtualbox_res/screenlayout.sh
+https://github.com/plimdim/bash-scripts/blob/65423cd74915ecb202564c0b12a2c2c705ec5e5f/change_virtualbox_res/screenlayout.sh
